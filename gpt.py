f4dc2962caf71b5d3e44dc9ecdd0616989cefd10
Amos You <amosyou21@gmail.com>
2024-01-02 01:04:57 -0800

    added feedforward

--- gpt.py
+++ gpt.py
@@ -91,6 +91,19 @@ class MultiHeadAttention(nn.Module):
     def forward(self, x):
         return torch.cat([h(x) for h in self.heads], dim=-1) # concat over channel dim
     
+class FeedForward(nn.Module):
+    """a simple linear layer followed by a non-linearity"""
+    
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, n_embd),
+            nn.ReLU(),
+        )
+        
+    def forward(self, x):
+        return self.net(x)
+    
 # super simple bigram model
 class BigramLanguageModel(nn.Module):
 
@@ -100,6 +113,7 @@ class BigramLanguageModel(nn.Module):
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
         self.sa_heads = MultiHeadAttention(4, n_embd // 4)
+        self.ffwd = FeedForward(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size) # language modeling head
 
     def forward(self, idx, targets=None):
@@ -110,6 +124,7 @@ class BigramLanguageModel(nn.Module):
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T, C)
         x = tok_emb + pos_emb # (B, T, C) - broadcasted across batch
         x = self.sa_heads(x) # apply one head of self-attention. (B, T, C)
+        x = self.ffwd(x) # (B, T, C)
         logits = self.lm_head(x) # (B, T, vocab_size)
         # logits = self.token_embedding_table(idx) # (B,T,C)
 
